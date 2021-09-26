e0bdb1c519e10f975ec91c6fd881a7eeccd1666d
Mohit Patil <86141072+bbeatz808@users.noreply.github.com>
2021-09-26 17:16:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// const navBtnContainer = document.querySelector(".nav-btn-container");
+// const userBtn = document.querySelector(".user-btn");
+
+// userBtn.addEventListener("click", () => {
+//     navBtnContainer.classList.add("active");
+//   });
